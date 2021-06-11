0b98f727d0496dd50e7af46871137e4548208b53
CatarinaGamboa <catarina.vg@hotmail.com>
2021-06-11 12:30:28 +0100

    Change starting value of TrafficLight

--- part3-liquidJava/together1/src/together1/Test2.java
+++ part3-liquidJava/together1/src/together1/Test2.java
@@ -7,16 +7,16 @@ public class Test2 {
 
 	public static void main(String[] args) throws IOException{
 		/*Uncomment Below*/
-		// ArrayDeque<Integer> p = new ArrayDeque<>();
-		// p.add(2);
-		// p.remove();
-		// p.offerFirst(6);
-		// p.getLast();
-		// p.remove();
-		// p.getLast();
-		// p.add(78);
-		// p.add(8);
-		// p.getFirst();
+		ArrayDeque<Integer> p = new ArrayDeque<>();
+		p.add(2);
+		p.remove();
+		p.offerFirst(6);
+		p.getLast();
+		p.remove();
+		p.getLast();
+		p.add(78);
+		p.add(8);
+		p.getFirst();
 		
 	}
 	
