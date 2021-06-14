822fd0dd659fab60462ad146b052ec6ec924593b
CatarinaGamboa <catarina.vg@hotmail.com>
2021-06-14 23:24:05 +0100

    Indent together1

--- part3-liquidJava/together1/src/together1/Test2.java
+++ part3-liquidJava/together1/src/together1/Test2.java
@@ -8,17 +8,17 @@ public class Test2 {
 
 	public static void main(String[] args) throws IOException{
 		/*Uncomment Below*/
-//		 ArrayDeque<Integer> p = new ArrayDeque<>();
-//		 p.add(2);
-//		 p.remove();
-//		 p.offerFirst(6);
-//		 p.getLast();
-//		 p.remove();
-//	   	 p.getLast();
-//		 p.add(78);
-//		 p.add(8);
-//		 p.getFirst();
+        // ArrayDeque<Integer> p = new ArrayDeque<>();
+        // p.add(2);
+        // p.remove();
+        // p.offerFirst(6);
+        // p.getLast();
+        // p.remove();
+        // p.getLast();
+        // p.add(78);
+        // p.add(8);
+        // p.getFirst();
 		
-	}
+    }
 	
 }
