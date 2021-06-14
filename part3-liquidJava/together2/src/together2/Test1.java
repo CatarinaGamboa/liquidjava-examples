efc2cd81bde6eea7f01068744985cefe02ac87b2
Catarina Gamboa <52540187+CatarinaGamboa@users.noreply.github.com>
2021-06-14 22:26:57 +0100

    Indent

--- part3-liquidJava/together2/src/together2/Test1.java
+++ part3-liquidJava/together2/src/together2/Test1.java
@@ -6,13 +6,13 @@ import repair.regen.specification.RefinementAlias;
 @RefinementAlias("Nat(int x) {x >= 0}")
 public class Test1 {
 	
-    /**
-    * The sum of all numbers between 0 and n
-    * @param n
-    * @return a positive value that represents the sum of all numbers between 0 and n
-    */
+    	/**
+    	* The sum of all numbers between 0 and n
+    	* @param n
+    	* @return a positive value that represents the sum of all numbers between 0 and n
+   	 */
 	/* Uncomment Below */
-    @Refinement("Nat(_) && _ >= n")
+    	@Refinement("Nat(_) && _ >= n")
 	public static int sum(int n) {
 		if(n <= 1)
 			return 0;
