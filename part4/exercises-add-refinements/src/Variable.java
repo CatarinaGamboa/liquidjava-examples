c30f0bbe9d6ee1b0eba6acac8a1809198d806df3
Catarina Gamboa <52540187+CatarinaGamboa@users.noreply.github.com>
2021-06-14 22:28:14 +0100

    Indent

--- part4/exercises-add-refinements/src/Variable.java
+++ part4/exercises-add-refinements/src/Variable.java
@@ -5,7 +5,7 @@ public class Variable {
     public static void main(String[] args) {
 		
 		/* A month needs to have a value between 1 and 12*/
-        int currentMonth;
+      	 	int currentMonth;
 
 		currentMonth = 13; //Error - Comment after finishing exercise
 		currentMonth = 5;  //Correct
