2e3cec9ec76fe97def89e8f795783cdba1ea0a4f
CatarinaGamboa <catarina.vg@hotmail.com>
2021-06-14 20:06:10 +0100

    Comment all java files for part3

--- part3-liquidJava/together2/src/together2/Test2.java
+++ part3-liquidJava/together2/src/together2/Test2.java
@@ -9,13 +9,13 @@ public class Test2 {
 	
 	public static void main(String[] args) throws IOException{
 		/* Uncomment Below */
-		int port = 5000;
-		InetAddress inetAddress = InetAddress.getByName("localhost");    
+		// int port = 5000;
+		// InetAddress inetAddress = InetAddress.getByName("localhost");    
 		
-		Socket socket = new Socket();
-		socket.bind(new InetSocketAddress(inetAddress, port));
-		socket.sendUrgentData(90);
-		socket.close();
+		// Socket socket = new Socket();
+		// socket.bind(new InetSocketAddress(inetAddress, port));
+		// socket.sendUrgentData(90);
+		// socket.close();
 
 
 
