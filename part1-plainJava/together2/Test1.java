3a829399fc64bc288774645a0fcc9a25734be1ed
CatarinaGamboa <catarina.vg@hotmail.com>
2021-06-16 11:43:11 +0100

    Change fibonacci in plainJava

--- part1-plainJava/together2/Test1.java
+++ part1-plainJava/together2/Test1.java
@@ -1,15 +1,16 @@
 public class Test1 {
     /**
     * Computes the fibonacci of index n
-    * @param n The index of the required fibonacci number
-    * @return The fibonacci nth number. The fibonacci sequence follows the formula Fn = Fn-1 + Fn-2 and has the starting values of F0 = 1 and F1 = 1 
+    * @param n The index of the required fibonacci number (greater or equal to 0)
+    * @return The fibonacci nth number. The fibonacci sequence follows the formula 
+              Fn = Fn-1 + Fn-2 and has the starting values of F0 = 0 and F1 = 1 
     */
     /*Uncomment Below*/
     public static int fibonacci(int n){
-        if(n <= 0)
+        if(n <= 1)
             return 0;
         else
-            return n + fibonacci(n-1);
+            return fibonacci(n-1) + fibonacci(n-2);
     }
 
 }
