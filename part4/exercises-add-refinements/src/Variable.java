04c44b0dd9f23c7504068c52289eb30dc0b9d074
CatarinaGamboa <catarina.vg@hotmail.com>
2021-06-16 11:44:25 +0100

    Improve indentation

--- part4/exercises-add-refinements/src/Variable.java
+++ part4/exercises-add-refinements/src/Variable.java
@@ -2,13 +2,13 @@ import repair.regen.specification.Refinement;
 
 public class Variable {
     
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 		
 		/* A month needs to have a value between 1 and 12*/
-        int currentMonth;
+    int currentMonth;
 
-        currentMonth = 13; //Error - Comment after finishing exercise
-        currentMonth = 5;  //Correct
+    currentMonth = 13; //Error - Comment after finishing exercise
+    currentMonth = 5;  //Correct
 
 	}
     
