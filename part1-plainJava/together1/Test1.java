9214c6aed94ee0be111bb88c46c6ae1d148e25fe
Catarina Gamboa <52540187+CatarinaGamboa@users.noreply.github.com>
2021-06-17 15:29:14 +0100

    Update Test1.java

--- part1-plainJava/together1/Test1.java
+++ part1-plainJava/together1/Test1.java
@@ -3,7 +3,7 @@ public class Test1 {
 	/**
 	 * The sum of all numbers between 0 and n
 	 * @param n
-	 * @return a positive value that represents the sum of all numbers between 0 and n
+	 * @return a positive value that represents the sum of all numbers between 0 and n, or 0 if n is negative
 	 */
 	public static int sum(int n) {
         if(n <= 1)
